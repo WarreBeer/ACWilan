b4f398b5ecc511f1a0e824f6dc38f98689a70d1a
Vanherck Ward <Ward.Vanherck@bankvanbreda.be>
2018-11-22 20:10:34 +0100

    Cleanup

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -21,7 +21,6 @@ namespace ACWilan.iOS
             // Override point for customization after application launch.
             // If not required for your application you can safely delete this method
             Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
-            SQLitePCL.CurrentPlatform.Init();
 
             return true;
         }
