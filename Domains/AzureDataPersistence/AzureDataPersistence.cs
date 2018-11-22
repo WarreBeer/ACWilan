b3f48c5c1a8538ba9bb0b18c7ab69dd3684e46f1
Vanherck Ward <Ward.Vanherck@bankvanbreda.be>
2018-11-22 19:32:49 +0100

    CleanUp

--- Domains/AzureDataPersistence/AzureDataPersistence.cs
+++ Domains/AzureDataPersistence/AzureDataPersistence.cs
@@ -6,22 +6,6 @@ namespace AzureDataPersistence
     public class AzureDataPersistence
     {
         public MobileServiceClient MobileService { get; set; }
-        IMobileServiceSyncTable coffeeTable;
 
-        public async Task Initialize()
-        {
-        }
-
-        public async Task<IEnumerable> GetActors()
-        {
-        }
-
-        public async Task AddActor(string fullName)
-        {
-        }
-
-        public async Task SyncActor()
-        {
-        }
     }
 }
